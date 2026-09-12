b405bd745ee4ae2c8a5bc63622ed0fa7cc4d1bdd
Cuppy <cuppy@users.noreply.github.com>
2026-09-13 05:05:54 +0800

    fix: use portable BusyBox bind mount syntax

--- scripts/console-entrypoint.sh
+++ scripts/console-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Test-only: runc rejects CLI mounts under /proc; mount in this private namespace.
 set -eu
-mount --bind /ld_unix_link/console-cmdline /proc/cmdline
-mount -o remount,bind,ro /proc/cmdline
+mount -o bind /ld_unix_link/console-cmdline /proc/cmdline
+[ "$(cat /proc/cmdline)" = 'console=ttyS0,115200n8' ]
 exec /usr/bin/landscape-start
